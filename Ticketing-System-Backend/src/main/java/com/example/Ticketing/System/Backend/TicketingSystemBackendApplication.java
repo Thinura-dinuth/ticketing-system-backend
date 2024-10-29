605c5c4f1ff2843bfb4ec628789b176a24e63e31
SeZo <60493233+Thinura-dinuth@users.noreply.github.com>
2024-10-29 14:52:37 +0530

    Created project and made the config file with parameters

---
+++ Ticketing-System-Backend/src/main/java/com/example/Ticketing/System/Backend/TicketingSystemBackendApplication.java
@@ -0,0 +1,14 @@
+package com.example.Ticketing.System.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketingSystemBackendApplication.class, args);
+	}
+
+
+}
